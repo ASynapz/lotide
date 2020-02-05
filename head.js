ca22771682784cb88a70197e0b53b521c9e65828
ASynapz <59207578+ASynapz@users.noreply.github.com>
2020-02-05 13:40:02 -0800

    Add files via upload

--- head.js
+++ head.js
@@ -1,8 +1,15 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const head = function(thisArray){
+  return thisArray[0];
+};
+
+
+assertEqual(head([]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
