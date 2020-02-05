ab759a86402e7dd5492e395229a819f2df8dbeb0
ASynapz <59207578+ASynapz@users.noreply.github.com>
2020-02-05 14:28:43 -0800

    Add files via upload

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -12,9 +12,9 @@ const eqArrays = function(arr1, arr2) {
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-    console.log("😄😄😄Assertion Passed: ", arr1, " === ", arr2);
+    console.log("Assertion Passed: ", arr1, " === ", arr2);
   } else {
-    console.log("😓😓😓Assertion Failed: ", arr1, " !== ", arr2);
+    console.log("Assertion Failed: ", arr1, " !== ", arr2);
   }
 };
 
