a1d311d607a64339a929e001ffde89bcdec0d1ad
ASynapz <59207578+ASynapz@users.noreply.github.com>
2020-02-05 13:42:34 -0800

    Add files via upload

--- tail.js
+++ tail.js
@@ -1,8 +1,14 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const tail = function(anArray) {
+  return anArray.slice(1);
+}
+const words = ['thing 1', 'thing 2', 'thing 3']
+console.log(tail(words));
+assertEqual(words.length, 3);
