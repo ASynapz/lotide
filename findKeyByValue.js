3f151051986793eaa7299cf164971ffd0e8aa5fb
ASynapz <59207578+ASynapz@users.noreply.github.com>
2020-02-05 14:59:39 -0800

    Add files via upload

---
+++ findKeyByValue.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, string) {
+  let output;
+  console.log(object);
+  for (const value in object) {
+    output = matchValue(object, value, string);
+    if (output !== undefined) {
+      return output;
+    }
+  }
+};
+
+const matchValue = function(object, value, string) {
+  console.log(`${string} comparing ${object[value]}`);
+  if (string === object[value]) {
+    console.log(`hit: ${string} comparing ${object[value]}`);
+    return value;
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  sitcom: "That '70s Show",
+  drama: "The Wire",
+  fantasy: "The Mandalorian"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), "sitcom");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Mandalorian"), "fantasy");
