cb872d32b5d4502d883949fa8820333e3f79b996
Alan Wu <59207578+ASynapz@users.noreply.github.com>
2020-04-02 14:04:47 -0700

    Add files via upload

--- map.js
+++ map.js
@@ -12,9 +12,9 @@ const eqArrays = function(arr1, arr2) {
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-    console.log("Assertion Passed: ", arr1, " === ", arr2);
+    console.log("😊😊😊Assertion Passed: ", arr1, " === ", arr2);
   } else {
-    console.log("Assertion Failed: ", arr1, " !== ", arr2);
+    console.log("😭😭😭Assertion Failed: ", arr1, " !== ", arr2);
   }
 };
 
@@ -26,7 +26,7 @@ const map = function(array, callback) {
     results.push(callback(item));
   }
   return results;
-};
+}
 
 const results1 = map(words, word => word[0]);
 console.log(results1);
