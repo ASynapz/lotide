9c52119a1d833a60bb1ce68ee61e45b788cb4b87
Alan Wu <59207578+ASynapz@users.noreply.github.com>
2020-04-06 13:10:04 -0700

    Add files via upload

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😊😊😊Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`😭😭😭Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length != arr2.length) {
@@ -18,6 +12,4 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-
-assertEqual(eqArrays([1, 2, "3"], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
