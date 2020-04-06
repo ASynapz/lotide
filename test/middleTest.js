96c63c9a4ac21a5da28b667ab651200968b237f7
Alan Wu <59207578+ASynapz@users.noreply.github.com>
2020-04-06 13:51:39 -0700

    Add files via upload

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,24 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const eqArrays = require('../eqArrays');
-const middle = require('../middle');
+const middle = require('../middle')
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([3, 4, 5, 6, 7, 8]), [5, 6]);
-assertArraysEqual(middle([4, 5, 6, 7, 8]), [5, 6]);
-assertArraysEqual(middle([4, 5, 6, 7, 8]), [6]);
+describe("#middle", () => {
+  it("returns [] of [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+  
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,19 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
+ const tail=require("../tail");
+const assert = require('chai').assert;
+const result = ["Hello", "Lighthouse", "Labs"];
+const words = ["Yo Yo", "Lighthouse", "Labs", "Jello"];
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+describe("#tail", () => {
+  it("returns Array for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.isArray(tail(["Hello", "Lighthouse", "Labs"]), ['Lighthouse', 'Labs']);
+  });
+
+  it("returns new array of length 2 for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(tail(result).length, 2);
+  });
+
+  it("returns new array of length 3 ['Yo Yo', 'Lighthouse', 'Labs', 'Jello']", () => {
+    assert.strictEqual(tail(words).length, 3);
+  });
+  
+});
